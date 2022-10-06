243dcd45cbee84b46bcff35744b31cd6f664ba9f
vitor lopes <lopesvitor959@gmail.com>
2022-10-06 18:16:51 +0100

    Single Product HTML structure

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -2,8 +2,6 @@ import React, {useState} from 'react';
 import { Form, Button } from 'react-bootstrap';
 import Rating from './Rating';
 
-
-
 const Filters = () => {
 
   const [rate, setRate] = useState(3)
--- src/components/SingleProduct.js
+++ src/components/SingleProduct.js
@@ -1,8 +1,45 @@
-import React from 'react'
+import React from 'react';
+import '../styles.css';
+import { Card, Button } from 'react-bootstrap';
+import Rating from './Rating';
 
-const SingleProduct = () => {
+const SingleProduct = ({prod}) => {
+
   return (
-    <div>SingleProduct</div>
+    <div className='products'>
+      <Card>
+        <Card.Img variant="top" src={prod.image} alt={prod.name} />     
+
+        <Card.Body>
+
+          <Card.Title>{prod.name}</Card.Title>
+
+          <Card.Subtitle style={{paddingBottom: 10}}>
+            <span>€ {prod.price.split(".")[0]} </span>
+            {
+              prod.fastDelivery
+              ?
+               <div>Fast Delivery</div>
+              :
+               <div>4 Days Delivery</div> 
+            }
+            <Rating rating={prod.ratings}/>
+          </Card.Subtitle>
+
+          <Button variant='danger'>
+            Remove from Cart
+          </Button>
+          
+          <Button
+            disabled={prod.inStock === 0}
+          >
+            {prod.inStock === 0 ? "Out of Stock" : "Add to Cart"}
+          </Button>
+
+        </Card.Body>
+
+      </Card>
+    </div>
   )
 }
 
